065ca62da6de224b07e8363f21a500d3dc71c5d5
Tim Borglund <dic13tbo@venus-5.student.lth.se>
2015-11-18 16:23:29 +0100

    More changes to the Server and WriteThread

--- src/server/Server.java
+++ src/server/Server.java
@@ -4,38 +4,48 @@ import java.io.IOException;
 import java.net.ServerSocket;
 import java.net.Socket;
 
+import se.lth.cs.eda040.fakecamera.AxisM3006V;
+
 public class Server {
+	private AxisM3006V camera;
 	private ReadThread rt;
 	private WriteThread wt;
 	private ServerSocket serverSocket;
 	private Socket clientSocket;
 	
-	public Server(){
+	public Server() throws IOException{
+		camera = new AxisM3006V();
+		camera.init();
+		camera.setProxy("argus-1.student.lth.se", 80); //ändra andressen
+		serverSocket = new ServerSocket(80);
+	}
+	
+	public static void main(String[] args){
 		try {
-			serverSocket = new ServerSocket(80);
-			execute();
-		} catch (IOException e) {
-			e.printStackTrace();
+			Server s = new Server();
+			s.execute();
+		} catch(IOException e) {
+			System.out.println("Error!");
+			System.exit(1);
 		}
 	}
 	
-	public void execute(){
+	public void execute() throws IOException{
 		while(true) {
-			try {
-				clientSocket = serverSocket.accept();
-				rt = new ReadThread(this, clientSocket.getInputStream()); // request 1
-				rt.start();
-				clientSocket.close();
-			} catch (IOException e) {
-				e.printStackTrace();
-			}
+			clientSocket = serverSocket.accept();
+			rt = new ReadThread(this, clientSocket.getInputStream()); // request 1
+			rt.start();
+			//clientSocket.close(); skall flyttas
 		}
 			
 	}
 	
 	public void requestRecieved(){
 		try {
-			wt = new WriteThread(this, clientSocket.getOutputStream());
+			if (!camera.connect()) {
+				System.out.println("Failed to connect to camera!");
+			}
+			wt = new WriteThread(camera, clientSocket.getOutputStream());
 			wt.start();
 		} catch (IOException e) {
 			e.printStackTrace();
--- src/server/WriteThread.java
+++ src/server/WriteThread.java
@@ -8,18 +8,22 @@ import se.lth.cs.eda040.fakecamera.AxisM3006V;
 public class WriteThread extends Thread {
 	
 	private OutputStream os;
-	private Server server; //Behövs nog inte
+	private AxisM3006V camera;
 	
-	public WriteThread(Server server, OutputStream os){
+	public WriteThread(AxisM3006V camera, OutputStream os){
 		this.os = os;
-		this.server = server;
+		this.camera = camera;
 	}
 	
 	public void run(){
-		byte[] jpeg = new byte[AxisM3006V.IMAGE_BUFFER_SIZE];
+		byte[] jpeg = new byte[AxisM3006V.IMAGE_BUFFER_SIZE + 10];
+		jpeg[0] = (byte) 1;
+		jpeg[1] = (byte) (camera.motionDetected() ? 1 : 0);
+		camera.getTime(jpeg, 2);
+		int len = camera.getJPEG(jpeg, 2+AxisM3006V.TIME_ARRAY_SIZE);
 		try {
 			os.write(jpeg);
-			os.flush();
+			os.flush(); //flushar den innan klienten hinner läsa den?
 		} catch (IOException e) {
 			e.printStackTrace();
 		}
