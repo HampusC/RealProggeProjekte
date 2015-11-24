7a31744e507ecb1aff2f5c5f12fd13753a4d6e38
Hampus Christensen <dat13hch@falk-8.student.lth.se>
2015-11-24 16:15:45 +0100

    fixed some image handling stuff

--- src/client/ClientReadThread.java
+++ src/client/ClientReadThread.java
@@ -2,6 +2,7 @@ package client;
 
 import java.io.IOException;
 import java.io.InputStream;
+import java.nio.ByteBuffer;
 import java.util.Arrays;
 
 import se.lth.cs.eda040.fakecamera.AxisM3006V;
@@ -19,13 +20,15 @@ private int cameraIndex;
 		while(true){
 			
 		
-			int maxToRead= AxisM3006V.IMAGE_BUFFER_SIZE + 10;
+			int maxToRead= AxisM3006V.IMAGE_BUFFER_SIZE + 10; // should be + 14
+			System.out.println(maxToRead);
 		byte[] buffer = new byte[maxToRead];
 		int read = 0;
 		int result = 0;
 		while (read<maxToRead && result!=-1) {
 		 try {
 			result = input.read(buffer,read,maxToRead-read);
+			System.out.println("recieved 1");
 		} catch (IOException e) {
 			// TODO Auto-generated catch block
 			e.printStackTrace();
@@ -35,6 +38,7 @@ private int cameraIndex;
 		}
 		//bufffer logic
 		if(buffer[0] == 1){ // vilkor = är bild
+			//System.out.println("img detected");
 			boolean motionDetected;
 			if(buffer[1]==1){
 				motionDetected=true;  
@@ -43,13 +47,20 @@ private int cameraIndex;
 				motionDetected=false;  
 				}
 			long timestamp =0; //ngra av bytsen i buffer
-			int length = buffer[2];
-			for (int i = 3; i < 10; i++){
+		
+			ByteBuffer b = ByteBuffer.allocate(4);
+			b.put(buffer, 2, 4);
+			b.position(0);
+			int length =b.getInt();
+			//System.out.println("recievied length = " +length);
+			
+			for (int i = 6; i < 14; i++){
 			   timestamp = (timestamp << 8) + (buffer[i] & 0xff);
 			}
-			byte[] image = Arrays.copyOfRange(buffer, 10, length+10); //ngra av bytsen i buffer
+			byte[] image = Arrays.copyOfRange(buffer, 14, length+14); //ngra av bytsen i buffer
+			//System.out.println(" recived: first bit in pic " + image[0]+ " last byte " + image[image.length-1]);
 			camH.writeToBuffer(timestamp, motionDetected, image , cameraIndex);
-			System.out.println("recieved");
+			//System.out.println("recieved");
 		}
 		camH.confirmRead();
 	}
--- src/server/WriteThread.java
+++ src/server/WriteThread.java
@@ -2,30 +2,51 @@ package server;
 
 import java.io.IOException;
 import java.io.OutputStream;
+import java.nio.ByteBuffer;
 
 import se.lth.cs.eda040.fakecamera.AxisM3006V;
 
 public class WriteThread extends Thread {
-	
+
 	private OutputStream os;
 	private AxisM3006V camera;
 	private ServerMonitor sm;
-	
-	public WriteThread(AxisM3006V camera, OutputStream os, ServerMonitor sm){
+
+	public WriteThread(AxisM3006V camera, OutputStream os, ServerMonitor sm) {
 		this.os = os;
 		this.camera = camera;
-		this.sm=sm;
+		this.sm = sm;
 	}
-	
-	public void run(){
-		byte[] jpeg = new byte[AxisM3006V.IMAGE_BUFFER_SIZE + 10]; //kanske image buffer size inte är längden av bilden (getJPEG kanske är istället)
-		while(true) {
+
+	public void run() {
+		byte[] jpeg = new byte[AxisM3006V.IMAGE_BUFFER_SIZE + 10]; // kanske
+																	// image
+																	// buffer
+																	// size inte
+																	// är
+																	// längden
+																	// av bilden
+																	// (getJPEG
+																	// kanske är
+																	// istället)
+		while (true) {
 			sm.shouldSend();
 			jpeg[0] = (byte) 1;
 			jpeg[1] = (byte) (camera.motionDetected() ? 1 : 0);
-			camera.getTime(jpeg, 3);
-			int len = camera.getJPEG(jpeg, 3 + AxisM3006V.TIME_ARRAY_SIZE);
-			jpeg[2] = (byte) len;
+			camera.getTime(jpeg, 2);
+			int len = camera.getJPEG(jpeg, 6 + AxisM3006V.TIME_ARRAY_SIZE);
+			System.out.println("first bit in pic " + jpeg[6 + AxisM3006V.TIME_ARRAY_SIZE] + " last byte " + jpeg[6 + AxisM3006V.TIME_ARRAY_SIZE + len]);
+			ByteBuffer b = ByteBuffer.allocate(4);
+			
+			// b.order(ByteOrder.BIG_ENDIAN); // optional, the initial order of
+			// a byte buffer is always BIG_ENDIAN.
+			b.putInt(len);
+			byte[] result = b.array();
+			for (int i = 0; i < 4; i++) {
+				jpeg[i+2] = result[i];
+			}
+
+			System.out.println(len );
 			try {
 				os.write(jpeg);
 				os.flush(); // flushar den innan klienten hinner läsa den?
